a682179fe80932a2162283758b29b59952fac0d8
Maddie <105728964+FreshRipeCherry@users.noreply.github.com>
2024-03-16 16:53:09 -0500

    fuck testworld.gd all my homies hate
    Removed the bits of code trying to add the crosshair, since the crosshair now it's a 3D object and not a UI element

--- Kleo Game Scenes/testworld.gd
+++ Kleo Game Scenes/testworld.gd
@@ -1,13 +1,8 @@
 extends Node3D
 
-@onready var crosshair = $UI/crosshair
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	#crosshair.position.x = get_viewport().size.x / 2 - 9
-	#crosshair.position.y = get_viewport().size.y / 2 - 9
-#WHY WON'T IT FUCKING WORK
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
