9e79611f46c3a601e7a1f8de1d4498884fecaeab
olive <162511422+oily-olive@users.noreply.github.com>
2024-04-10 18:59:37 -0400

    relocated scripts to correct folder

---
+++ Kleo Game Scripts/bullet_trail.gd
@@ -0,0 +1,29 @@
+extends MeshInstance3D
+
+@onready var gunImpact = $HitImpact
+var alpha = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var duplicate_material = material_override.duplicate()
+	material_override = duplicate_material
+
+func init(posBarrel, posTarget):
+	var draw_mesh = ImmediateMesh.new()
+	mesh = draw_mesh
+	draw_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material_override)
+	draw_mesh.surface_add_vertex(posBarrel)
+	draw_mesh.surface_add_vertex(posTarget)
+	draw_mesh.surface_end()
+
+func _process(delta):
+	alpha = alpha - (delta * 3.5)
+	material_override.albedo_color.a = alpha
+
+func trigger_particle(pos, barrelPosition): #reminder to check the enemy scan
+	gunImpact.position = pos
+	gunImpact.look_at(barrelPosition)
+	gunImpact.emitting = true
+
+func _on_timer_timeout():
+	queue_free()
