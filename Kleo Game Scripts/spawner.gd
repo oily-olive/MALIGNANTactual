a99890993b94cda415fa88cb8bef1a3e94e0bfbd
olive <162511422+oily-olive@users.noreply.github.com>
2024-05-15 17:21:18 -0400

    Add files via upload

--- Kleo Game Scripts/spawner.gd
+++ Kleo Game Scripts/spawner.gd
@@ -3,17 +3,20 @@ class_name Spawner
 
 @export var thing_to_spawn : PackedScene
 @export var spawns_immediately : bool
+@export var Node_to_inherit_from : Node3D
 var load_thing
 
 signal spawn_signal()
 
 func _ready():
 	connect("spawn_signal", spawn)
+	if Node_to_inherit_from == null:
+		Node_to_inherit_from = self
 	if spawns_immediately:
 		spawn()
 
 func spawn():
 	load_thing = thing_to_spawn.instantiate()
-	load_thing.position = self.global_position
-	load_thing.rotation = self.global_rotation
+	load_thing.position = Node_to_inherit_from.global_position
+	load_thing.rotation = Node_to_inherit_from.global_rotation
 	get_tree().current_scene.add_child.call_deferred(load_thing)
